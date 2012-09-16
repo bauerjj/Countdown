096ac083e76b26508fab21e42c5abd0203afb3b8
Michael Stuckey <sikeston11@hotmail.com>
2012-09-15 23:29:21 -0700

    Refactor decrement to halt count at zero

--- counter.c
+++ counter.c
@@ -31,89 +31,85 @@ void display(void) {
 void decrement(void) {
 
     if (segments.sec.l == 0) {
-        segments.sec.l = 9;
-
+        
         if (segments.sec.h == 0) {
-            segments.sec.h = 5;
-
+            
             // -- begin decrement minutes
             if (segments.min.l == 0) {
-                segments.min.l = 9;
-
+                
                 if (segments.min.h == 0) {
-                    segments.min.h = 5;
-
+                    
                     // -- begin decrement hours
-
-                    if (segments.hour.l == 0){
-                        if(segments.hour.h == 0)
-                             segments.hour.l = 3;
-                        else
-                            segments.hour.l = 9;
+                    if (segments.hour.l == 0) {
 
                         if (segments.hour.h == 0) {
-                            segments.hour.h = 2;
-
+                            
                             // -- begin decrement days
                             if (segments.day.l == 0) {
-                                segments.day.l = 9;
-
+                                
                                 if (segments.day.h == 0) {
-                                    segments.day.h = 3;
-
+                                    
                                     // -- begin decrement months
                                     if (segments.month.l == 0) {
-                                        segments.month.l = 9;
-
+                                        
                                         if (segments.month.h == 0) {
-                                            segments.month.h = 3; // Note: This assumes 30 days in each month
-
+                                            
                                             // -- begin decrement years
                                             if (segments.year.l == 0) {
-                                                segments.year.l = 9;
-
+                                                
                                                 if (segments.year.h == 0) {
                                                     // Counter has hit zero
-                                                    
+                                                    return; // effectively holds count at 0
                                                 } else {
                                                     segments.year.h--;
                                                 }
+                                                segments.year.l = 9;
                                             } else {
                                                 segments.year.l--;
                                             }
-
                                             // -- end decrement years
+                                            segments.month.h = 3; // Note: This assumes 30 days in each month
                                         } else {
                                             segments.month.h--;
                                         }
+                                        segments.month.l = 9;
                                     } else {
-                                            segments.month.l--;
+                                        segments.month.l--;
                                     }
                                     // -- end decrement months
+                                    segments.day.h = 3;
                                 } else {
                                     segments.day.h--;
                                 }
+                                segments.day.l = 9;
                             } else {
                                 segments.day.l--;
                             }
                             // -- end decrement days
+                            segments.hour.h = 2;
+                            segments.hour.l = 3;
                         } else {
                             segments.hour.h--;
                         }
+                        segments.hour.l = 9;
                     } else {
-                            segments.hour.l--;
+                        segments.hour.l--;
                     }
                     // -- end decrement hours
+                    segments.min.h = 5;
                 } else {
                     segments.min.h--;
                 }
+                segments.min.l = 9;
             } else {
                 segments.min.l--;
             }
             // -- end decrement minutes
+            segments.sec.h = 5;
         } else {
             segments.sec.h--;
         }
+        segments.sec.l = 9;
     } else {
         segments.sec.l--;
     }
